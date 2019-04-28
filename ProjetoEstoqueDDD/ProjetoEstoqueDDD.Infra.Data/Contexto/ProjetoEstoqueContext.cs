41d7db55aff7091b61d99c2510ecbe1a7bbf35cf
MuriloSGomes <seno.murilo@hotmail.com>
2019-04-28 14:42:36 -0300

    Configuração das entidades, limitando alguns atributos.

---
+++ ProjetoEstoqueDDD/ProjetoEstoqueDDD.Infra.Data/Contexto/ProjetoEstoqueContext.cs
@@ -0,0 +1,50 @@
+﻿using ProjetoEstoqueDDD.Domain.Entities;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+
+namespace ProjetoEstoqueDDD.Infra.Data.Contexto
+{
+    public class ProjetoEstoqueContext : DbContext
+    {
+        public ProjetoEstoqueContext()
+            : base("ProjetoEstoqueDDD")
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Properties().Where(propertie => propertie.Name == propertie.ReflectedType.Name + "Id")
+                .Configure(p => p.IsKey());
+
+            modelBuilder.Properties<string>().Configure(p => p.HasColumnType("varchar"));
+
+            modelBuilder.Properties<string>().Configure(p => p.HasMaxLength(100));
+
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+                if(entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+            return base.SaveChanges();
+        }
+    }
+}
