2e192b20b70be659422e3f11d59d936636f1c669
MuriloSGomes <seno.murilo@hotmail.com>
2019-04-28 14:40:54 -0300

    Adiciona classe de cliente e suas propriedades.

---
+++ ProjetoEstoqueDDD/ProjetoEstoqueDDD.Domain/Entities/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ProjetoEstoqueDDD.Domain.Entities
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+        public bool ClienteEspecial(Cliente cliente)
+        {
+            //Aquele cliente antigo, que ja ta a mais de 5 anos adiquirindo os serviços da empresa
+            return cliente.Ativo && DateTime.Now.Year - cliente.DataCadastro.Year >= 5;
+        }
+    }
+}
+
