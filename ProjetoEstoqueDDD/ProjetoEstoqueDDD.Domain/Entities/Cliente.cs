015f5908915c81254ed430542c7c59435180ed9d
MuriloSGomes <seno.murilo@hotmail.com>
2019-04-28 15:35:31 -0300

    Adiciona nova para propriedade referindo a um enumerable de produtos

--- ProjetoEstoqueDDD/ProjetoEstoqueDDD.Domain/Entities/Cliente.cs
+++ ProjetoEstoqueDDD/ProjetoEstoqueDDD.Domain/Entities/Cliente.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ProjetoEstoqueDDD.Domain.Entities
 {
@@ -10,6 +11,7 @@ namespace ProjetoEstoqueDDD.Domain.Entities
         public string Email { get; set; }
         public DateTime DataCadastro { get; set; }
         public bool Ativo { get; set; }
+        public virtual IEnumerable<Produto> Produtos { get; set; }
 
         public bool ClienteEspecial(Cliente cliente)
         {
