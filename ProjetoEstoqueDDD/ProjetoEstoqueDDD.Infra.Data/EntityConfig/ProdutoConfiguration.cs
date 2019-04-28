39386023c31fe14d58b8721cc02b26b79d043cbb
MuriloSGomes <seno.murilo@hotmail.com>
2019-04-28 15:44:18 -0300

    Adiciona classe de produto e produto configuration.

---
+++ ProjetoEstoqueDDD/ProjetoEstoqueDDD.Infra.Data/EntityConfig/ProdutoConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using ProjetoEstoqueDDD.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace ProjetoEstoqueDDD.Infra.Data.EntityConfig
+{
+    public class ProdutoConfiguration : EntityTypeConfiguration<Produto>
+    {
+        public ProdutoConfiguration()
+        {
+            HasKey(produto => produto.ProdutoId);
+
+            Property(produto => produto.Nome)
+                .IsRequired()
+                .HasMaxLength(250);
+
+            Property(produto => produto.Valor)
+                .IsRequired();
+
+            HasRequired(produto => produto.Cliente)
+                .WithMany()
+                .HasForeignKey(produto => produto.ClienteId);
+        }
+    }
+}
