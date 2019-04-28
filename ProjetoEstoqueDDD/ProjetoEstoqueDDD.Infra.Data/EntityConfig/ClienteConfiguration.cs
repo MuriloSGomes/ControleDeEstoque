5cc942cd9475f70e02525520ead424aa20667444
MuriloSGomes <seno.murilo@hotmail.com>
2019-04-28 15:14:54 -0300

    Cliente configuration

---
+++ ProjetoEstoqueDDD/ProjetoEstoqueDDD.Infra.Data/EntityConfig/ClienteConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using ProjetoEstoqueDDD.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace ProjetoEstoqueDDD.Infra.Data.EntityConfig
+{
+    public class ClienteConfiguration : EntityTypeConfiguration<Cliente>
+    {
+        public ClienteConfiguration()
+        {
+            HasKey(clienteId => clienteId.ClienteId);
+
+            Property(cliente => cliente.Nome)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            Property(cliente => cliente.SobreNome)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            Property(cliente => cliente.Email)
+                .IsRequired();
+        }
+    }
+}
