774223f8462f558748535b85fbdf8791a4f31e2e
MuriloSGomes <seno.murilo@hotmail.com>
2019-05-01 01:11:01 -0300

    adiciona interfaces no dominio

--- ProjetoEstoqueDDD/ProjetoEstoqueDDD.Domain/Interfaces/IRepositoryBase.cs
+++ ProjetoEstoqueDDD/ProjetoEstoqueDDD.Domain/Interfaces/IRepositoryBase.cs
@@ -1,7 +1,15 @@
-﻿namespace ProjetoEstoqueDDD.Domain.Interfaces
+﻿using System.Collections.Generic;
+
+namespace ProjetoEstoqueDDD.Domain.Interfaces
 {
     public interface IRepositoryBase<T> where T: class
     {
         void Add(T obj);
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        void Update(T obj);
+        void Remove(T obj);
+        void Dispose();
+
     }
 }
