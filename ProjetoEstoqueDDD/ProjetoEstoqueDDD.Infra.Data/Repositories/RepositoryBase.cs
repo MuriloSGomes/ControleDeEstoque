948556414a6e4a68498c129c99f81d86289e6630
MuriloSGomes <seno.murilo@hotmail.com>
2019-05-01 03:35:39 -0300

    Implementa classe de repositorio base adicionando todas as chamadas disponíveis.

---
+++ ProjetoEstoqueDDD/ProjetoEstoqueDDD.Infra.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,47 @@
+﻿using ProjetoEstoqueDDD.Domain.Interfaces;
+using ProjetoEstoqueDDD.Infra.Data.Contexto;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace ProjetoEstoqueDDD.Infra.Data.Repositories
+{
+    public class RepositoryBase<T> : IDisposable, IRepositoryBase<T> where T : class
+    {
+        protected ProjetoEstoqueContext Db = new ProjetoEstoqueContext();
+
+        public void Add(T obj)
+        {
+            Db.Set<T>().Add(obj);
+            Db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Dispose();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return Db.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return Db.Set<T>().Find(id);
+        }
+
+        public void Remove(T obj)
+        {
+            Db.Set<T>().Remove(obj);
+            Db.SaveChanges();
+        }
+
+        public void Update(T obj)
+        {
+            //Entry serve para garantir de que essa entidade ja existe no banco
+            Db.Entry(obj).State = EntityState.Modified;
+        }
+    }
+}
