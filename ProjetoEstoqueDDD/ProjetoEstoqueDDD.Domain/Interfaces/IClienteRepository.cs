4353a0aafecbef599095bdc7ebd3ff8c9d9460d4
MuriloSGomes <seno.murilo@hotmail.com>
2019-05-01 09:33:37 -0300

    Ajusta para interface e não class.

--- ProjetoEstoqueDDD/ProjetoEstoqueDDD.Domain/Interfaces/IClienteRepository.cs
+++ ProjetoEstoqueDDD/ProjetoEstoqueDDD.Domain/Interfaces/IClienteRepository.cs
@@ -1,39 +1,9 @@
 ﻿using ProjetoEstoqueDDD.Domain.Entities;
-using System;
-using System.Collections.Generic;
 
 namespace ProjetoEstoqueDDD.Domain.Interfaces
 {
-    public class IClienteRepository : IRepositoryBase<Cliente>
+    public interface IClienteRepository : IRepositoryBase<Cliente>
     {
-        public void Add(Cliente obj)
-        {
-            throw new System.NotImplementedException();
-        }
 
-        public void Dispose()
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<Cliente> GetAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Cliente GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Remove(Cliente obj)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(Cliente obj)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
