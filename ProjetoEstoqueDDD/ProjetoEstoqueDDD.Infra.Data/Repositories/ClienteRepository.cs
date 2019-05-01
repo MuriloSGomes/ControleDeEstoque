76f2b9075980e158bd95fb868c9b5205614aeee8
MuriloSGomes <seno.murilo@hotmail.com>
2019-05-01 09:51:53 -0300

    Adiciona repositorio de cliente e produto

---
+++ ProjetoEstoqueDDD/ProjetoEstoqueDDD.Infra.Data/Repositories/ClienteRepository.cs
@@ -0,0 +1,10 @@
+﻿using ProjetoEstoqueDDD.Domain.Entities;
+using ProjetoEstoqueDDD.Domain.Interfaces;
+
+namespace ProjetoEstoqueDDD.Infra.Data.Repositories
+{
+    public class ClienteRepository : RepositoryBase<Cliente>, IClienteRepository
+    {
+
+    }
+}
---
+++ ProjetoEstoqueDDD/ProjetoEstoqueDDD.Infra.Data/Repositories/ProdutoRepository.cs
@@ -0,0 +1,17 @@
+﻿using ProjetoEstoqueDDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEstoqueDDD.Infra.Data.Repositories
+{
+    public class ProdutoRepository : RepositoryBase<Produto>
+    {
+        public IEnumerable<Produto> BuscarPorNome(string nome)
+        {
+            return Db.Produtos.Where(produto => produto.Nome == nome);
+        }
+    }
+}
