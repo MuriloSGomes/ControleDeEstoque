513248d7caa4e8c7c9053ef27a6e740281e4dab2
MuriloSGomes <seno.murilo@hotmail.com>
2019-05-01 03:36:14 -0300

    Adiciona nova interface para produto

---
+++ ProjetoEstoqueDDD/ProjetoEstoqueDDD.Domain/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,9 @@
+﻿using ProjetoEstoqueDDD.Domain.Entities;
+using System.Collections;
+
+namespace ProjetoEstoqueDDD.Domain.Interfaces
+{
+    public interface IProdutoRepository: IRepositoryBase<Produto>
+    {
+    }
+}
