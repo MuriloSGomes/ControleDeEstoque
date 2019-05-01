77fa9d146b857d5cbbafc733ca000b01626f29ae
MuriloSGomes <seno.murilo@hotmail.com>
2019-05-01 09:43:31 -0300

    Implementa pesquisa na interface de produto

--- ProjetoEstoqueDDD/ProjetoEstoqueDDD.Domain/Interfaces/IProdutoRepository.cs
+++ ProjetoEstoqueDDD/ProjetoEstoqueDDD.Domain/Interfaces/IProdutoRepository.cs
@@ -1,9 +1,11 @@
 ﻿using ProjetoEstoqueDDD.Domain.Entities;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace ProjetoEstoqueDDD.Domain.Interfaces
 {
     public interface IProdutoRepository: IRepositoryBase<Produto>
     {
+        IEnumerable<Produto> BuscarPorNome(string nome);
     }
 }
